b001eb822021e2997b4ed83f29c47fdb57ff02a5
Janusz Piechówka <januszpiechowka@gmail.com>
2022-12-28 17:58:19 +0100

    Bind to random port on startup

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
+use std::net::TcpListener;
+
 use rusty_newsletter::run_server;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    run_server()?.await
+    let tcp_listener = TcpListener::bind("127.0.0.1:0").expect("Failed to create TCP listener");
+    run_server(tcp_listener)?.await
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,11 +1,12 @@
+use std::net::TcpListener;
+
 #[tokio::test]
 async fn health_check_works() {
-    spawn_app();
-
+    let address = spawn_app();
     let http_client = reqwest::Client::new();
 
     let response = http_client
-        .get("http://127.0.0.1:8000/health_check")
+        .get(&format!("{}/health_check", &address))
         .send()
         .await
         .expect("Failed to send request");
@@ -14,7 +15,13 @@ async fn health_check_works() {
     assert_eq!(Some(0), response.content_length());
 }
 
-fn spawn_app() {
-    let server = rusty_newsletter::run_server().expect("Failed to start server for testing");
+fn spawn_app() -> String {
+    // Port zero will provide random port from the OS
+    let tcp_listener = TcpListener::bind("127.0.0.1:0").expect("Failed to create TCP listener");
+    let port = tcp_listener.local_addr().unwrap().port();
+    let server =
+        rusty_newsletter::run_server(tcp_listener).expect("Failed to start server for testing");
     let _ = tokio::spawn(server);
+
+    format!("http://127.0.0.1:{}", port)
 }
