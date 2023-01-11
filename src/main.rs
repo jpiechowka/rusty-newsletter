2a41adc6d0bb1cb2c5a263728203cb190c5cc84c
Janusz Piechówka <januszpiechowka@gmail.com>
2023-01-11 17:44:11 +0100

    Add structured logging / tracing

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,24 @@
 use rusty_newsletter::{config::get_config, startup::run_server};
 use sqlx::PgPool;
 use std::net::TcpListener;
+use tracing::subscriber::set_global_default;
+use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
+use tracing_log::LogTracer;
+use tracing_subscriber::{prelude::__tracing_subscriber_SubscriberExt, EnvFilter, Registry};
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
+    LogTracer::init().expect("Failed to initialize logger");
+
+    let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
+    let formatting_layer = BunyanFormattingLayer::new("rusty-newsletter".into(), std::io::stdout);
+    let tracing_subscriber = Registry::default()
+        .with(env_filter)
+        .with(JsonStorageLayer)
+        .with(formatting_layer);
+
+    set_global_default(tracing_subscriber).expect("Failed to set tracing subscriber");
+
     let config = get_config().expect("Failed to read application configuration");
     let db_conn_pool = PgPool::connect(&config.db_settings.connection_string())
         .await
--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -13,6 +13,19 @@ pub async fn subscribe(
     form: web::Form<SubscribeFormData>,
     db_conn_pool: web::Data<PgPool>,
 ) -> HttpResponse {
+    let request_id = Uuid::new_v4();
+    tracing::info!(
+        "request_id {} - Adding '{}' '{}' as a new newsletter subscriber",
+        request_id,
+        form.email,
+        form.name
+    );
+
+    tracing::info!(
+        "request_id {} - Saving new subscriber details in the database",
+        request_id
+    );
+
     match sqlx::query!(
         r#"
         INSERT INTO subscriptions (id, email, name, subscribed_at)
@@ -26,9 +39,19 @@ pub async fn subscribe(
     .execute(db_conn_pool.as_ref())
     .await
     {
-        Ok(_) => HttpResponse::Ok().finish(),
+        Ok(_) => {
+            tracing::info!(
+                "request_id {} - New subscriber details have been saved",
+                request_id
+            );
+            HttpResponse::Ok().finish()
+        }
         Err(e) => {
-            println!("Failed to execute query: {}", e);
+            tracing::error!(
+                "request_id {} - Failed to execute query: {:?}",
+                request_id,
+                e
+            );
             HttpResponse::InternalServerError().finish()
         }
     }
