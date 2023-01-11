3d6dbac9a47e16110b662c38079cb366d36d07d6
Janusz Piechówka <januszpiechowka@gmail.com>
2023-01-11 17:55:14 +0100

    Refactor and move tracing code to telemetry.rs file

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,16 @@
-use rusty_newsletter::{config::get_config, startup::run_server};
+use rusty_newsletter::{
+    config::get_config,
+    startup::run_server,
+    telemetry::{get_tracing_subscriber, init_tracing_subscriber},
+};
 use sqlx::PgPool;
 use std::net::TcpListener;
-use tracing::subscriber::set_global_default;
-use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
-use tracing_log::LogTracer;
-use tracing_subscriber::{prelude::__tracing_subscriber_SubscriberExt, EnvFilter, Registry};
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    LogTracer::init().expect("Failed to initialize logger");
-
-    let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
-    let formatting_layer = BunyanFormattingLayer::new("rusty-newsletter".into(), std::io::stdout);
-    let tracing_subscriber = Registry::default()
-        .with(env_filter)
-        .with(JsonStorageLayer)
-        .with(formatting_layer);
-
-    set_global_default(tracing_subscriber).expect("Failed to set tracing subscriber");
+    let tracing_subscriber =
+        get_tracing_subscriber("rusty-newsletter".into(), "info".into(), std::io::stdout);
+    init_tracing_subscriber(tracing_subscriber);
 
     let config = get_config().expect("Failed to read application configuration");
     let db_conn_pool = PgPool::connect(&config.db_settings.connection_string())
