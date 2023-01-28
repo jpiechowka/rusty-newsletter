5d3c00c00858590ec82e63fd8e971f465fd313b9
Janusz Piechówka <januszpiechowka@gmail.com>
2023-01-28 13:56:31 +0100

    Initial code for SubscriberName

--- src/domain.rs
+++ src/domain.rs
@@ -1,1 +1,19 @@
+use unicode_segmentation::UnicodeSegmentation;
+
+const FORBIDDEN_CHARACTERS: [char; 9] = ['/', '(', ')', '"', '<', '>', '\\', '{', '}'];
+
 pub struct SubscriberName(String);
+
+impl SubscriberName {
+    pub fn parse(s: String) -> SubscriberName {
+        let is_empty_or_whitespace = s.trim().is_empty();
+        let is_too_long = s.graphemes(true).count() > 256;
+        let contains_forbidden_characters = s.chars().any(|c| FORBIDDEN_CHARACTERS.contains(&c));
+
+        if is_empty_or_whitespace || is_too_long || contains_forbidden_characters {
+            panic!("{s} is not a valid subscriber name")
+        } else {
+            Self(s)
+        }
+    }
+}
