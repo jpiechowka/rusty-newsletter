32795f1d9c25f357fe16a0b4b44ecb3dc9bec16c
Janusz Piechówka <januszpiechowka@gmail.com>
2023-02-05 21:05:34 +0100

    Use reqwest when creating new EmailClient

--- src/email_client.rs
+++ src/email_client.rs
@@ -1,10 +1,22 @@
+use reqwest::Client;
+
 use crate::domain::SubscriberEmail;
 
 pub struct EmailClient {
     sender: SubscriberEmail,
+    base_url: String,
+    http_client: Client,
 }
 
 impl EmailClient {
+    pub fn new(base_url: String, sender: SubscriberEmail) -> Self {
+        Self {
+            sender,
+            base_url,
+            http_client: Client::new(),
+        }
+    }
+
     pub async fn send_email(
         &self,
         recipient: SubscriberEmail,
